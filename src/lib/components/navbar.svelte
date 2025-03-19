3f893c9370d518056d9b0a44c5bbdf3eed3a6e9c
shap-po <slavko.ukrop@gmail.com>
2025-03-19 10:24:58 +0200

    fix: Navigation not working on GitHub pages

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,6 +2,7 @@
     import {page} from '$app/state';
     import {Dropdown, DropdownItem, Navbar, NavLi, NavUl} from "flowbite-svelte";
     import {ChevronDownOutline} from 'flowbite-svelte-icons';
+    import {base} from "$app/paths";
 
     let activeUrl = page.url.toString();
 </script>
@@ -13,15 +14,15 @@
             <ChevronDownOutline class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"/>
         </NavLi>
         <Dropdown>
-            <DropdownItem href="/ciphers/caesar">Caesar</DropdownItem>
-            <DropdownItem href="/ciphers/vigenere">Vigenere</DropdownItem>
+            <DropdownItem href="{base}/ciphers/caesar">Caesar</DropdownItem>
+            <DropdownItem href="{base}/ciphers/vigenere">Vigenere</DropdownItem>
         </Dropdown>
         <NavLi>
             Tools
             <ChevronDownOutline class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"/>
         </NavLi>
         <Dropdown>
-            <DropdownItem href="/tools/frequency-analysis">Frequency Analysis</DropdownItem>
+            <DropdownItem href="{base}/tools/frequency-analysis">Frequency Analysis</DropdownItem>
         </Dropdown>
     </NavUl>
 </Navbar>
